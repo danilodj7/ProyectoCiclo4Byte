0929cef74506bf452a165f38e074bb16f86f4775
David Saavedra <david.d11@hotmail.com>
2021-11-20 21:30:50 -0500

    agregando modelo de usuarios

--- index.ts
+++ index.ts
@@ -1,7 +1,32 @@
-import conectarBD from './db/db'
+import conectarBD from "./db/db";
+import { UserModel } from "./models/user";
+import { Enum_Rol } from "./models/enums";
 
 const main = async () => {
-    await conectarBD();
-}
+  await conectarBD();
+
+  // Crear usuario
+  await UserModel.create({
+    apellido: "Saavedra Diaz",
+    correo: "dsaav@correo.com",
+    identificacion: "1233456",
+    nombre: "Carlos",
+    rol: Enum_Rol.ADMINISTRADOR,
+  })
+    .then((u) => {
+      console.log("usuario creado", u);
+    })
+    .catch((e) => {
+      console.error("Error creando el usuario", e);
+    });
+
+  //   await UserModel.find()
+  //     .then((u) => {
+  //       console.log("usuarios", u);
+  //     })
+  //     .catch((e) => {
+  //       console.error("Error obteniendo los usuarios", e);
+  //     });
+};
 
 main();
---
+++ models/enums.ts
@@ -0,0 +1,7 @@
+enum Enum_Rol {
+  ESTUDIANTE = "ESTUDIANTE",
+  LIDER = "LIDER",
+  ADMINISTRADOR = "ADMINISTRADOR",
+}
+
+export { Enum_Rol };
