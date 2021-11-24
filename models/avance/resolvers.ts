c9ee1f510302d7887972a966070298625d56aa52
David Saavedra <david.d11@hotmail.com>
2021-11-23 23:26:34 -0500

    Agregando populate para query de avance

--- models/avance/resolvers.ts
+++ models/avance/resolvers.ts
@@ -3,18 +3,21 @@ import { ModeloAvance } from "./avance";
 const resolversAvance = {
   Query: {
     Avances: async (parent, args) => {
-      const avances = await ModeloAvance.find();
+      const avances = await ModeloAvance.find()
+        .populate("proyecto")
+        .populate("creadoPor");
       return avances;
     },
   },
   Mutation: {
     crearAvance: async (parent, args) => {
       const AvanceCreado = await ModeloAvance.create({
-        fecha: args.fecha,
+        fecha: new Date(args.fecha),
         descripcion: args.descripcion,
         proyecto: args.proyecto,
         creadoPor: args.creadoPor,
       });
+
       return AvanceCreado;
     },
   },
